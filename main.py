090bde3d48a63b41af1fdb7bcc19a81af28c1a2b
Alex Llanez <xljdev@proton.me>
2025-06-24 10:25:50 -0600

    feat: add options for user.

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+def main():
 
+    todos = []
+
+    while True:
+        user_action = input("Type add, show or exit:")
+        user_action = user_action.strip()
+
+        match user_action:
+            case "add":
+                todo = input("Enter a todo: ")
+                todos.append(todo)
+            case "show":
+                for item in todos:
+                    print(item)
+            case "exit":
+                break
+
+    print("Bye!")
+
+
+if __name__ == "__main__":
+    main()
