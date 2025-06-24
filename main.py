68e596cb1886af07c55928717446fbaca1a86cc4
Alex Llanez <xljdev@proton.me>
2025-06-24 12:45:36 -0600

    feat: add edit option for elements in the list

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def main():
     todos = []
 
     while True:
-        user_action = input("Type add, show or exit:")
+        user_action = input("Type add, show, edit or exit:")
         user_action = user_action.strip()
 
         match user_action:
@@ -13,6 +13,11 @@ def main():
             case "show":
                 for item in todos:
                     print(item)
+            case "edit":
+                number = int(input("Number of the todo to edit: "))
+                number = number - 1
+                new_todo = input("Enter new todo: ")
+                todos[number] = new_todo
             case "exit":
                 break
 
